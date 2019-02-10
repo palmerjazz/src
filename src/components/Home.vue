0080abd6da246861e537091ef72cd7f1d00a9967
Andrew Palmer <atp@freeshell.org>
2019-02-09 18:35:06 -0600

    Link home page to site pages

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <inner-item>Hello, world</inner-item>
+    <inner-item v-for="{ routeName, linkText } in siteSections" :key="routeName">
+      <div><router-link :to="{ name: routeName }">{{ linkText }}</router-link></div>
+    </inner-item>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      siteSections: [
+        { routeName: 'Audio', linkText: 'Arranging Audio' },
+        { routeName: 'Video', linkText: 'Arranging Video' },
+        { routeName: 'PianoSamples', linkText: 'Piano Samples' },
+      ],
+    };
+  },
+};
+</script>
